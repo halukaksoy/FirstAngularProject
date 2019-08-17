c5bdac8ff131f01f7ef2a6984630824b5701551a
DESKTOP-CKNRHSH\haluk <halukaksoy91@gmail.com>
2019-08-17 19:56:10 +0300

    Second

---
+++ src/app/works/works.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-works',
+  templateUrl: './works.component.html',
+  styleUrls: ['./works.component.css']
+})
+export class WorksComponent implements OnInit {
+
+  constructor() { }
+
+  loginUserName:string = "haluk.aksoy";
+
+  ngOnInit() {
+  }
+
+}
