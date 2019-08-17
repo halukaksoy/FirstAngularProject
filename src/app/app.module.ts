77684454c876fdb224c281b3873f2a8ea1de3eff
DESKTOP-CKNRHSH\haluk <halukaksoy91@gmail.com>
2019-08-17 19:53:02 +0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; 
+import { WorksComponent } from './works/works.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    WorksComponent
   ],
   imports: [
     BrowserModule
